5ada51ae84edb02e5cc16eba5a01bb8f4f39cdef
Bazmahou <bouissihamza6@gmail.com>
2022-04-19 05:29:35 +0100

    Status updated

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
-from typing import Optional
+from aws_xray_sdk.core import xray_recorder
 
-from fastapi import FastAPI
+from typing import Optional
+import requests
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World 2"}
+    return {"Hello": "World 6"}
 
-
+@app.get("/service_b")
+def read(ip:str):
+    res = requests.get(ip)
+    if res.ok:
+        return {"status":"ok"}
+    return {"status":"fail"}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
